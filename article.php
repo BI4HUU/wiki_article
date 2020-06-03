2b00683b34102451715df59b5bae4efd6c117b4e
BI4HUU <BI4HUU@yandex.ru>
2020-06-03 07:24:34 +0300

    Update article.php

--- article.php
+++ article.php
@@ -1,12 +1,26 @@
 <?php 
-$connect = new mysqli("127.0.0.1", "root", "root", "article");
-$result = $connect->query("SELECT * FROM `article` WHERE 1"); 
-include "header.php" ?>
+$mysqli = new mysqli("localhost", "id11565558_root", "o)!Z~v%+<CRjh^W0", "id11565558_article");
+if (!$mysqli) {
+	echo "Ошибка: Невозможно установить соединение с MySQL.";
+	exit;
+}
+$query =  "SELECT * FROM article WHERE `id`='$_GET["id"]'";
+	if ($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			echo ("<h1>" . $row['title'] . "</h1><p>" . $row['body'] . "</p><div class='author'>" . $row['name'] . "  " . $row['date'] . "</div>
+			<hr>");
+		}
 
-<section style="container">
-    
+	$result->free();
+}
+?>
+</section>
+<?php include "footer.php" ?>
+
+
+
 <?php 
-    while ($row = $result->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
 		echo  '<h1>' . $row["title"] . '</h1>';
 		echo  $row["description"];
 		echo  $row["keywords"];
@@ -16,6 +30,3 @@ include "header.php" ?>
 
 	}
 ?>
-
-</section>
-<?php include "footer.php" ?>
